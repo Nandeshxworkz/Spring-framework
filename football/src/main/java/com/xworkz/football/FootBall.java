2934954302c7ffefd3950fc12856b6ec068a5158
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-08-25 01:20:51 +0530

    FootBall example of using wrapperClasses

---
+++ football/src/main/java/com/xworkz/football/FootBall.java
@@ -0,0 +1,42 @@
+package com.xworkz.football;
+
+public class FootBall {
+	
+	private String brand;
+	private Double price;
+	
+	
+	
+	public FootBall() {
+		System.out.println(this.getClass().getSimpleName());
+	}
+
+
+	public String getBrand() {
+		return brand;
+	}
+
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FootBall [brand=" + brand + ", price=" + price + "]";
+	}
+	
+	
+
+}
